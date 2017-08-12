836725119801fdf98f8f268a66f8ed8adbc72756
hzb <xthzb@qq.com>
2017-08-12 11:40:42 +0800

    t1

--- CTool/MainForm.Designer.cs
+++ CTool/MainForm.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace CTool {
-    partial class Form1 {
+    partial class MainForm {
         /// <summary>
         /// 必需的设计器变量。
         /// </summary>
--- CTool/MainForm.cs
+++ CTool/MainForm.cs
@@ -8,8 +8,8 @@ using System.Text;
 using System.Windows.Forms;
 
 namespace CTool {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class MainForm : Form {
+        public MainForm() {
             InitializeComponent();
         }
     }
